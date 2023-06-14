d4c6c10df2dce78c74fb47d2c7abd32d8a0ba915
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2023-06-14 15:09:51 +0500

    Add files via upload

---
+++ 9-pass_by_value & pass_by_reference/practice_9/practice_9/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_9
+{
+    /*
+    // Pass By Value 
+    internal class Program
+    {
+        static void passbyValue(int new_Value)
+        {
+            new_Value += 5;
+            Console.WriteLine("New Value is {0} ", new_Value);
+        }
+        static void Main(string[] args)
+        {
+            int old_value = 10;
+            passbyValue(old_value);
+            Console.WriteLine("Old value is {0} ", old_value);
+    */
+/*
+//Pass By Reference
+internal class Program
+{
+    static void passbyRef(ref int new_Value)
+    {
+        new_Value += 5;
+        Console.WriteLine("New Value is {0} ", new_Value);
+    }
+    static void Main(string[] args)
+    {
+        int old_value = 10;
+        passbyRef(ref old_value);
+        Console.WriteLine("Old value is {0} ", old_value);
+*/
+
+/*
+//Pass By Reference
+  internal class Program
+  {
+    static void value_increment(ref int x) 
+    { 
+        x++;
+        Console.WriteLine(x);
+    }
+    static void Main(string[] args)
+    {
+        int a = 5;
+        value_increment(ref a);
+        Console.WriteLine(a);
+*/
+        Console.ReadLine();
+
+    }
+  }
+}
